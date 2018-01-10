f24175b0c6d05e4f3f3c57f935477ef072450cd0
Wouter van Atteveldt <wouter@vanatteveldt.com>
2018-01-10 17:26:47 +0100

    Fix error on None status attr

--- orange3sma/progress.py
+++ orange3sma/progress.py
@@ -3,20 +3,22 @@ from progressmonitor import ProgressMonitor
 
 
 def progress_monitor(widget: OWWidget, status_attr=None, start_msg='Started', done_msg='Done') -> ProgressMonitor:
+    def status(msg):
+        if status_attr and hasattr(widget, status_attr):
+            setattr(widget, status_attr, msg)
+
     def on_progress_update(m: ProgressMonitor):
         if m.is_done:
             widget.progressBarFinished()
-            if hasattr(widget, status_attr):
-                setattr(widget, status_attr, done_msg)
+            if done_msg:
+                status(done_msg)
         else:
             widget.progressBarSet(m.progress * 100)
-            if hasattr(widget, status_attr):
-                setattr(widget, status_attr, m.message)
-
+            status(m.message)
 
     widget.progressBarInit()
-    if hasattr(widget, status_attr):
-        setattr(widget, status_attr, start_msg)
+    if start_msg:
+        status(start_msg)
     m = ProgressMonitor()
     m.add_listener(on_progress_update)
     return m
